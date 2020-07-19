1c1177ca4d7b8f224cec3e03f39fa4d7c3828e36
Leonardo <leonardo.kanashiro.felizardo@gmail.com>
2020-07-19 20:05:05 -0300

    Home

--- escalariqueza/views.py
+++ escalariqueza/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 
+def home(*args, **kwargs):
+    return HttpResponse("<h1>Home Page<h1>")
 
 def index(request):
     return HttpResponse("Escala de Riqueza no Brasil")
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from escalariqueza import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('escalariqueza/', include('escalariqueza.urls')),
     path('admin/', admin.site.urls),
 ]
